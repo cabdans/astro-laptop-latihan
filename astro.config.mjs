28b6badae5eab55640d26572ac70d6f3ebeff463
cahya <cahyaabdan633@gmail.com>
2026-06-08 10:50:02 +0700

    fix: ganti redirect Astro.redirect (invalid SSG) dengan astro.config redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/review/budget/advan-workplus-r5-6600h': '/review/produktivitas/advan-workplus-r5-6600h',
+    '/review/budget/advan-workplus-heritage-r5-7535hs': '/review/produktivitas/advan-workplus-heritage-r5-7535hs',
+  }
+});
